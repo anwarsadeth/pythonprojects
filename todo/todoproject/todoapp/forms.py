44dd73379285db35aa5e669df9dabfeeda8d1c2f
anwarsadeth <anwarsadethsulaiman@gmail.com>
2022-06-11 12:07:34 +0530

    added

---
+++ todo/todoproject/todoapp/forms.py
@@ -0,0 +1,13 @@
+from .models import Task
+from django import forms
+
+class todoform(forms.ModelForm):
+    class Meta:
+        model=Task
+        fields=['name','priority','date']
+
+
+
+
+
+
---
+++ todo/todoproject/todoapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+
+    path('',views.demo,name='demo'),
+    # path('details',views.details,name='details')
+    path('delete/<int:id>/',views.delete,name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.Taskdetailview.as_view(),name='cbvdeatil'),
+    path('cbvupdate/<int:pk>/',views.Taskupdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.Taskdeleteview.as_view(),name='cbvdelete'),
+]
